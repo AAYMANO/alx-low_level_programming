71b24c477219f732bdd71905e21f44d4d6c189eb
ayman <aymanairout7@gmail.com>
2023-06-15 11:28:55 -0700

    printf.c

--- 0x00-hello_world/printf.c
+++ 0x00-hello_world/printf.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
-
 /**
- * main - prints "whith proper grammar, but the outcome is 
- * a piece of art," flowwed newline
- * Return: Always 0.
- *
+ * main - a C program that prints a new line using printf
+ * Return: 0 (Success)
  */
 int main(void)
-
-{
-	printf("with proper grammar,but the outcome is a piece of art,\n");
-	return (0);
-}
+{  printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0); }
