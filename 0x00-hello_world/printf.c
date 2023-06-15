09dbd555d0fb8a4d81a396bff9f8b7f28e4a2e82
ayman <aymanairout7@gmail.com>
2023-06-15 11:11:10 -0700

    ll

---
+++ 0x00-hello_world/printf.c
@@ -0,0 +1,13 @@
+#include <stdio	.h>
+/**
+ * main - prints "whith proper grammar, but the outcome is
+ * a piace of art," followed newline
+ * return: Always 0.
+ *
+ */
+int main(void)
+
+{
+	printf("whith proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
